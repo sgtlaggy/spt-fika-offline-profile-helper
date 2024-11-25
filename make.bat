462502184ff0836cb483e07c2295fcb74b04bf0d
sgtlaggy <8661717+sgtlaggy@users.noreply.github.com>
2024-11-24 17:37:36 -0800

    make the batch script more robust

--- make.bat
+++ make.bat
@@ -1,2 +1,22 @@
 @echo off
-pyinstaller -F OfflineProfileHelper.pyw
+
+where /q py
+if errorlevel 1 (
+   echo Python is not installed or `py` launcher is not available in PATH.
+
+   choice /m "Open python.org to download now"
+   if errorlevel 1 (
+      start "" https://python.org/downloads
+   )
+
+   exit /B
+)
+
+if not exist venv\ (
+   py -m venv venv
+)
+
+call venv\Scripts\activate.bat
+
+py -m pip install -Ur exe-requirements.txt
+py -m PyInstaller -Fw OfflineProfileHelper.pyw
